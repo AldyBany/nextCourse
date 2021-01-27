c4cb86d715d29589d1b02cead0155507d9141104
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2021-01-27 22:27:15 +0200

    init

---
+++ next-tutorial/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WedDev Newz</title>
+        
+      </Head>
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
